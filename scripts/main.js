224fd89de13a3abfc04e97b2092273c629ec8e3d
Tori Franklin <toripfranklin@gmail.com>
2023-01-26 23:39:34 -0500

    Array Methods

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,11 @@ import { renderToDom } from "../utils/renderToDom.js";
 // .forEach()
 const renderCards = (array) => {
   let refStuff = "<h1 class='text-white'>Cards Go Here!</h1>";
+
+  array.forEach((item) => {
+    refStuff += card(item);
+  })
+
   renderToDom("#cards", refStuff);
 }
 
@@ -14,32 +19,54 @@ const renderCards = (array) => {
 // .findIndex() & (.includes() - string method)
 const toggleCart = (event) => {
   if (event.target.id.includes("fav-btn")) {
-   console.log('Clicked Fav btn')
+   const [, id] = event.target.id.split('--');
+
+   const index = referenceList.findIndex(item => item.id === Number(id))
+
+   referenceList[index].inCart = !referenceList[index].inCart;
+   cartTotal();
+   renderCards(referenceList);
   }
 }
+
 
 // SEARCH
 // .filter()
 const search = (event) => {
   const eventLC = event.target.value.toLowerCase();
-  console.log(eventLC)
+  const result = referenceList.filter(item => 
+    item.title.toLowerCase().includes(eventLC) ||
+    item.author.toLowerCase().includes(eventLC) ||
+    item.description.toLowerCase().includes(eventLC)
+  )
+  renderCards(result);
 }
 
 // BUTTON FILTER
 // .filter() & .reduce() &.sort() - chaining
+
 const buttonFilter = (event) => {
   if(event.target.id.includes('free')) {
-    console.log('FREE')
+    const free = referenceList.filter(item => item.price <= 0)
+    renderCards(free);
   }
   if(event.target.id.includes('cartFilter')) {
-    console.log('cartFilter')
+    const cart = referenceList.filter(item => item.inCart === true) // === true not needed but i like it in there bc it's clear what i'm filtering
+    renderCards(cart);
   }
+
   if(event.target.id.includes('books')) {
-    console.log('books!')
+    const books = referenceList.filter(item => item.type.toLowerCase() === "book");
+    // could also have written: (item => item.type === "Book");
+    // .toLowerCase will target a string/value no matter the case you enter
+    renderCards(books);
   }
+
   if(event.target.id.includes('clearFilter')) {
-    console.log('clearFilter')
+    renderCards(referenceList);
+    // because we just want clear filter to return all cards
   }
+
   if(event.target.id.includes('productList')) {
     let table = `<table class="table table-dark table-striped" style="width: 600px">
     <thead>
@@ -52,7 +79,8 @@ const buttonFilter = (event) => {
     <tbody>
     `;
     
-    productList().forEach(item => {
+    //LOOKUP
+    productList().sort((a,b) => a.type.localeCompare(b.type)).forEach(item => {
       table += tableRow(item);
     });
 
@@ -66,14 +94,26 @@ const buttonFilter = (event) => {
 // CALCULATE CART TOTAL
 // .reduce() & .some()
 const cartTotal = () => {
-  const total = 0
+  const cart = referenceList.filter(item => item.inCart);
+  const total = cart.reduce((a, b) => a + b.price, 0);
+  const free = cart.some(item => item.price <= 0);
   document.querySelector("#cartTotal").innerHTML = total.toFixed(2);
+
+  if (free) {
+    document.querySelector('#includes-free').innerHTML = 'INCLUDES FREE ITEMS'
+  } else {
+    document.querySelector('#includes-free').innerHTML = ''
+  }
 }
 
 // RESHAPE DATA TO RENDER TO DOM
 // .map()
 const productList = () => {
-  return [{ title: "SAMPLE TITLE", price: 45.00, type: "SAMPLE TYPE" }]
+  return referenceList.map(item => ({ 
+    title: item.title, 
+    price: item.price, 
+    type: item.type 
+  }))
 }
 
 
